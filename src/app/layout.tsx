4019d494db009bf28872e793e73d3fe45addc4d4
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-10 19:42:54 +0900

    #1 env: scss 폴더 구조 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import '@/styles/base/globals.scss';
 
 export const metadata: Metadata = {
   title: 'GiveMeFive!',
@@ -12,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   );
