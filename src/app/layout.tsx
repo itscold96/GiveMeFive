646e0238d16ac0dab8b1f13af28f88860c86c76c
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-10 19:48:03 +0900

    #1 env: 가변 다이나믹 서브셋 Pretandard 전역 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
       </head>
       <body>{children}</body>
     </html>
