7b6f6ea166d3afda530bce954c0ae05a981c81a9
ayoooyh <syh9566@naver.com>
2024-10-14 15:27:27 +0900

    #6 feat : 공용 컴포넌트 button 수정

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,5 +1,40 @@
 import S from './Button.module.scss';
+import { ButtonHTMLAttributes } from 'react';
 
+type ButtonProps = {
+  buttonWidth?: 'full';
+  buttonColor: 'nomadBlack' | 'white' | 'gray';
+  textSize: 'md' | 'lg';
+  borderRadius: 'radius6' | 'radius4';
+  padding: 'padding8' | 'padding14';
+  className?: string;
+  children: React.ReactNode;
+  isFull?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button() {
-  return <Button>Button</Button>;
+export default function Button({
+  // isFull,
+  buttonWidth,
+  buttonColor,
+  textSize,
+  borderRadius,
+  padding,
+  className,
+  children,
+  ...buttonAttributes
+}: ButtonProps) {
+  return (
+    <button
+      className={`${S.button} 
+        ${S[`${buttonWidth}ButtonWidth`]}
+        ${S[`${buttonColor}ButtonColor`]}
+        ${S[`${textSize}TextSize`]}
+        ${S[`${borderRadius}BorderRadius`]}
+        ${S[padding]}
+        ${className || ''}`}
+      {...buttonAttributes}
+    >
+      {children}
+    </button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Test from './test';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Test />
+    </div>
+  );
 }
