3a87feefcd9e829a3e3315224cbd8f4896e17632
ayoooyh <syh9566@naver.com>
2024-10-14 17:44:41 +0900

    #6 feat : 공용 컴포넌트 button 수정

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -2,19 +2,15 @@ import S from './Button.module.scss';
 import { ButtonHTMLAttributes } from 'react';
 
 type ButtonProps = {
-  buttonWidth?: 'full';
   buttonColor: 'nomadBlack' | 'white' | 'gray';
-  textSize: 'md' | 'lg';
+  textSize: 'md' | 'lg'; //md : 14px, lg : 16px
   borderRadius: 'radius6' | 'radius4';
   padding: 'padding8' | 'padding14';
   className?: string;
   children: React.ReactNode;
-  isFull?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button({
-  // isFull,
-  buttonWidth,
   buttonColor,
   textSize,
   borderRadius,
@@ -26,7 +22,6 @@ export default function Button({
   return (
     <button
       className={`${S.button} 
-        ${S[`${buttonWidth}ButtonWidth`]}
         ${S[`${buttonColor}ButtonColor`]}
         ${S[`${textSize}TextSize`]}
         ${S[`${borderRadius}BorderRadius`]}
