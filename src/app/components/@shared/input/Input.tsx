5cb1e6c127c6cf0fa37fc3538eeb3ab1c05a1ec3
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-14 12:54:51 +0900

    #2 feat: 공용 Input 컴포넌트 제작

---
+++ src/app/components/@shared/input/Input.tsx
@@ -0,0 +1,39 @@
+import classNames from 'classnames';
+import S from './Input.module.scss';
+import { FieldError, FieldErrorsImpl, Merge, UseFormRegisterReturn } from 'react-hook-form';
+import { HTMLInputTypeAttribute } from 'react';
+
+interface ValidInputProps {
+  label?: string;
+  htmlFor?: string;
+  error?: FieldError | Merge<FieldError, FieldErrorsImpl>;
+  message?: string | FieldError | Merge<FieldError, FieldErrorsImpl>;
+  register?: UseFormRegisterReturn;
+  type?: HTMLInputTypeAttribute;
+  placeholder?: string;
+  onChange?: () => void;
+}
+
+export default function Input({
+  label,
+  htmlFor,
+  error,
+  message,
+  register,
+  type = 'text',
+  placeholder = '',
+}: ValidInputProps) {
+  return (
+    <div className={S.inputWrapper}>
+      {label && <label htmlFor={htmlFor}>{label}</label>}
+      <input
+        id={htmlFor}
+        type={type}
+        className={classNames({ [S.error]: error })}
+        placeholder={placeholder}
+        {...register}
+      />
+      {message && <p className={S.message}>{message.toString()}</p>}
+    </div>
+  );
+}
