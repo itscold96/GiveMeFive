eb962ffc37df284629847af492f7f9b831eff569
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-16 20:38:49 +0900

    #16 feat: axios.interceptor로 토큰 재발급 로직 구현

--- src/fetches/setupAxios.ts
+++ src/fetches/setupAxios.ts
@@ -1,6 +1,62 @@
 import axios from 'axios';
+import { getCookie, setCookie } from 'cookies-next';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
 });
+
+// 요청 interceptor
+axiosInstance.interceptors.request.use(config => {
+  const accessToken = getCookie('accessToken');
+
+  // accessToken이 없다면 그대로 전송
+  if (!accessToken) {
+    return config;
+  }
+
+  // accessToken이 있다면 header authorization에 담아서 전송
+  config.headers.authorization = `Bearer ${accessToken}`;
+  return config;
+});
+
+// 응답 interceptor
+axiosInstance.interceptors.response.use(
+  response => {
+    // 응답이 올바르게 온다면 그대로 return
+    return response;
+  },
+  async error => {
+    // 에러가 발생한다면 경우에 따라 refreshToken으로 accessToken을 재발급 받아 다시 요청
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    // 기존 요청 저장
+    const originalRequest = config;
+
+    // unauthorize 일 경우
+    if (status === 401 && error.response.data.message === 'Unauthorized') {
+      const refreshToken = getCookie('refreshToken');
+      // 토큰 재발급 요청
+      const { data } = await axiosInstance.post(
+        '/auth/tokens',
+        {},
+        { headers: { authorization: `Bearer ${refreshToken}` } },
+      );
+
+      // 재발급된 token으로 쿠키 업데이트
+      const { accessToken: newAccessToken, refreshToken: newRefreshToken } = data;
+      setCookie('accessToken', newAccessToken);
+      setCookie('newRefreshToken', newRefreshToken);
+
+      // 401로 실패했던 요청을 새로운 accessToken으로 재요청
+      originalRequest.headers.authorization = `Bearer ${newAccessToken}`;
+      return axios(originalRequest);
+    }
+
+    // Token 문제 외의 다른 문제일 경우 그대로 진행
+    return axios(originalRequest);
+  },
+);
--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -1,19 +1,17 @@
 import { UserStore } from '@/types/user';
 import { create } from 'zustand';
 
-export const useUserStore = create<UserStore>(set => {
-  return {
-    user: null,
-    action: {
-      setUser: newUser => {
-        set(() => ({ user: newUser }));
-      },
-      updateProfileImageUrl: newImageUrl => {
-        set(prevState => ({ ...prevState, profileImageUrl: newImageUrl }));
-      },
-      resetUser: () => {
-        set(() => ({ user: null }));
-      },
+export const useUserStore = create<UserStore>(set => ({
+  user: null,
+  action: {
+    setUser: newUser => {
+      set(() => ({ user: newUser }));
     },
-  };
-});
+    updateProfileImageUrl: newImageUrl => {
+      set(prevState => ({ ...prevState, profileImageUrl: newImageUrl }));
+    },
+    resetUser: () => {
+      set(() => ({ user: null }));
+    },
+  },
+}));
