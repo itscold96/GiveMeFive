62e63433aacbf3b3e9b25f5e80da6ba42c2d9849
ayoooyh <syh9566@naver.com>
2024-10-18 02:10:43 +0900

    #17 feat : activities 수정

--- src/api/activities.ts
+++ src/api/activities.ts
@@ -1,25 +1,45 @@
 import { axiosInstance } from './axiosInstance';
 
-export type Activities = {
+export interface Activity {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface Activities {
   cursorId: number;
   totalCount: number;
-  activities: {
-    id: number;
-    userId: number;
-    title: string;
-    description: string;
-    category: string;
-    price: number;
-    address: string;
-    bannerImageUrl: string;
-    rating: number;
-    reviewCount: number;
-    createdAt: Date;
-    updatedAt: Date;
-  }[];
-};
+  activities: Activity[];
+}
 
-export const getActivities = async (): Promise<Activities> => {
-  const response = await axiosInstance.get('/activities');
+const getActivities = async ({
+  category,
+  sort,
+  // method: 'offset',
+  keyword,
+}: {
+  category: '문화 · 예술' | '식음료' | '스포츠' | '투어' | '관광' | '웰빙';
+  sort: 'most_reviewed' | 'price_asc' | 'price_desc' | 'latest';
+  // method: 'offset' | 'cursor';
+  keyword?: string;
+}): Promise<Activities> => {
+  const response = await axiosInstance.get(`/activities`, {
+    params: {
+      category,
+      sort,
+      method: 'offset',
+      keyword,
+    },
+  });
   return response.data;
 };
+
+export { getActivities };
