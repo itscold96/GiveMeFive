e54d666369532f34c400aab85f62cd9c945dc2a2
ayoooyh <syh9566@naver.com>
2024-10-17 23:02:51 +0900

    #17 feat : 카테고리 분리 및 수정

--- src/app/(contents)/main/category/category.tsx
+++ src/app/(contents)/main/category/category.tsx
@@ -1,14 +1,14 @@
-import S from './category.module.scss';
+import S from './Category.module.scss';
+
+const categoryList = ['문화 · 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
 
 export default function Category() {
-  const categoryList = ['문화 · 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
-
   return (
     <div className={S.categoryContainer}>
       {categoryList.map(category => (
-        <div key={category} className={S.category}>
+        <button key={category} className={S.category}>
           {category}
-        </div>
+        </button>
       ))}
     </div>
   );
