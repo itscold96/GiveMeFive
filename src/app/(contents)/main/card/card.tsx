3b2249d832dd7b86fb71604207eae7fdc824277f
ayoooyh <syh9566@naver.com>
2024-10-17 13:23:49 +0900

    #17 feat : card api 불러오기 수정

--- src/app/(contents)/main/card/card.tsx
+++ src/app/(contents)/main/card/card.tsx
@@ -1,9 +1,13 @@
 import S from './card.module.scss';
+import Image from 'next/image';
+import { Activities } from '@/api/activities';
 
-export default function Card() {
+export default function Card({ activities }: { activities: Activities }) {
   return (
     <div className={S.cardContainer}>
-      <div className={`${S.card} ${S.cardLarge}`}>card</div>
+      <div className={`${S.card} ${S.cardLarge}`}>
+        {/* <Image src={activities.activities[0].bannerImageUrl} alt="card" /> */}
+      </div>
       <div className={`${S.card} ${S.cardLarge}`}>card</div>
       <div className={`${S.card} ${S.cardLarge}`}>card</div>
     </div>
