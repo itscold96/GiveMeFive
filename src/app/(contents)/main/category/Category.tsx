5e7cb13f706a876d3cb7c4b0c1866485d4edbc1d
ayoooyh <syh9566@naver.com>
2024-10-18 02:12:20 +0900

    #17 feat : 카테고리, 드롭다운 병합 및 수정

--- src/app/(contents)/main/category/Category.tsx
+++ src/app/(contents)/main/category/Category.tsx
@@ -1,15 +1,46 @@
 import S from './Category.module.scss';
+import Dropdown from '../../../components/@shared/dropdown/Dropdown';
+import useDropdown from '@/hooks/useDropdown';
 
-const categoryList = ['문화 · 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
+export type Category = '문화 · 예술' | '식음료' | '스포츠' | '투어' | '관광' | '웰빙';
+const categoryList: Category[] = ['문화 · 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
 
-export default function Category() {
+interface CategoryProps {
+  selectedCategory: Category;
+  setSelectedCategory: (category: Category) => void;
+  selectedSort: string;
+  handleSortChange: (value: string) => void;
+}
+
+export default function Category({ selectedCategory, setSelectedCategory }: CategoryProps) {
+  const dropdownData = ['가격이 낮은 순', '가격이 높은 순'];
+  const { data, selectedValue, onDropdownChange } = useDropdown(dropdownData);
+
+  const displayValue = selectedValue || '가격';
+
+  switch (selectedValue) {
+    case '가격이 낮은 순':
+      break;
+    case '가격이 높은 순':
+      break;
+  }
+
   return (
-    <div className={S.categoryContainer}>
-      {categoryList.map(category => (
-        <button key={category} className={S.category}>
-          {category}
-        </button>
-      ))}
+    <div className={S.categoryDropdownContainer}>
+      <div className={S.categoryContainer}>
+        {categoryList.map(category => (
+          <button
+            key={category}
+            className={`${S.category} ${selectedCategory === category ? S.selected : ''}`}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className={S.dropdownContainer}>
+        <Dropdown data={data} onChange={onDropdownChange} selectedValue={displayValue} />
+      </div>
     </div>
   );
 }
