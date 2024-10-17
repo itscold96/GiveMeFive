fb0f8813c2dc5fa2636be10ee75b16f4ccd4564c
DasomKim <ekt0521@naver.com>
2024-10-17 14:41:49 +0900

    #18 Refactor

--- src/app/mypage/myprofile/page.tsx
+++ src/app/mypage/myprofile/page.tsx
@@ -2,36 +2,8 @@
 import React from 'react';
 import UserInfoForm from '../../components/mypage/UserInfoForm';
 import SideMenu from '../../components/mypage/SideMenu';
-<<<<<<< HEAD
 import Footer from '../../components/footer/Footer';
 import S from './myprofile.module.scss';
-=======
-import AlertModal from '../../components/@shared/modal/AlertModal';
-import ConfirmModal from '../../components/@shared/modal/ConfirmModal';
-import styles from '../mypage.module.scss';
-
-const Myprofile: React.FC = () => {
-  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
-  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
-
-  const openAlertModal = () => {
-    setIsAlertModalOpen(true);
-  };
-
-  const openConfirmModal = () => {
-    setIsConfirmModalOpen(true);
-  };
-
-  const handleAlert = () => {
-    console.log('Alerted');
-    //로직 추가
-  };
-
-  const handleConfirm = () => {
-    console.log('Confirmed');
-    //로직 추가
-  };
->>>>>>> 6d4568d3c47b868b8a6f021098adbe31a2272613
 
 function Myprofile() {
   return (
