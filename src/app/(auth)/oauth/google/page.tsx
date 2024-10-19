d72f1624e75dc249af7a2417c90aa10096200110
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-19 14:44:24 +0900

    #22 fix: getGoogleJWT 토큰에서 id_token를 추출해서 oauthSignin으로 전달

--- src/app/(auth)/oauth/google/page.tsx
+++ src/app/(auth)/oauth/google/page.tsx
@@ -17,17 +17,17 @@ export default function RedirectGoogle() {
 
   useEffect(() => {
     const fetcher = async () => {
-      const token = await getGoogleJWT(code);
+      const { id_token } = await getGoogleJWT(code);
 
       try {
-        const data = await oauthSignin({ provider: 'google', code: token });
+        const data = await oauthSignin({ provider: 'google', code: id_token });
         const { user, accessToken, refreshToken } = data as LoginReturn;
         setUser({ user, accessToken, refreshToken });
 
         router.replace('/');
       } catch (error) {
         if (error instanceof AxiosError && error.status === 403) {
-          router.replace(`/oauth/google/signup/${token}`);
+          router.replace(`/oauth/google/signup/${id_token}`);
         }
       }
     };
