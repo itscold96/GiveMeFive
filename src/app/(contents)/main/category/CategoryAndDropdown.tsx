ff8c7807d220371696ce836b179f52c9ebe323b6
ayoooyh <syh9566@naver.com>
2024-10-20 16:50:50 +0900

    #17 feat : 드롭다운 선택대로 정렬 수정

--- src/app/(contents)/main/category/CategoryAndDropdown.tsx
+++ src/app/(contents)/main/category/CategoryAndDropdown.tsx
@@ -5,6 +5,8 @@ import useDropdown from '@/hooks/useDropdown';
 export type Category = '문화 · 예술' | '식음료' | '스포츠' | '투어' | '관광' | '웰빙';
 const categoryList: Category[] = ['문화 · 예술', '식음료', '스포츠', '투어', '관광', '웰빙'];
 
+export type Sort = 'price_asc' | 'price_desc';
+const sortList: Sort[] = ['price_asc', 'price_desc'];
 interface CategoryProps {
   selectedCategory: Category;
   setSelectedCategory: (category: Category) => void;
@@ -12,18 +14,24 @@ interface CategoryProps {
   handleSortChange: (value: string) => void;
 }
 
-export default function CategoryAndDropdown({ selectedCategory, setSelectedCategory }: CategoryProps) {
-  const dropdownData = ['가격이 낮은 순', '가격이 높은 순'];
-  const { data, selectedValue, onDropdownChange } = useDropdown(dropdownData);
+export default function CategoryAndDropdown({
+  selectedCategory,
+  setSelectedCategory,
+  selectedSort,
+  handleSortChange,
+}: CategoryProps) {
+  const dropdownList = sortList.map(sort => (sort === 'price_asc' ? '가격이 낮은 순' : '가격이 높은 순'));
+  const { toggleDropdown, isDropdownToggle } = useDropdown(dropdownList);
 
-  const displayValue = selectedValue || '가격';
+  // const displayValue = selectedValue || '가격';
 
-  switch (selectedValue) {
-    case '가격이 낮은 순':
-      break;
-    case '가격이 높은 순':
-      break;
-  }
+  const handleDropdownChange = (value: string) => {
+    const sortValue = value === '가격이 낮은 순' ? 'price_asc' : 'price_desc';
+    handleSortChange(sortValue);
+  };
+
+  const displayValue =
+    selectedSort === 'price_asc' ? '가격이 낮은 순' : selectedSort === 'price_desc' ? '가격이 높은 순' : '가격';
 
   return (
     <div className={S.categoryDropdownContainer}>
@@ -39,7 +47,14 @@ export default function CategoryAndDropdown({ selectedCategory, setSelectedCateg
         ))}
       </div>
       <div className={S.dropdownContainer}>
-        <Dropdown data={data} onChange={onDropdownChange} selectedValue={displayValue} />
+        <Dropdown
+          data={dropdownList}
+          onChange={handleDropdownChange}
+          type="default"
+          isDropdownToggle={isDropdownToggle}
+          toggleDropdown={toggleDropdown}
+          selectedValue={displayValue}
+        />
       </div>
     </div>
   );
