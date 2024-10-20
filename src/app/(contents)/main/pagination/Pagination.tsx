a891fdbe074455064b6ab6e4808c66dd83e69bf5
ayoooyh <syh9566@naver.com>
2024-10-20 23:17:54 +0900

    #17 feat : pagenation 수정

--- src/app/(contents)/main/pagination/Pagination.tsx
+++ src/app/(contents)/main/pagination/Pagination.tsx
@@ -2,11 +2,16 @@ import { Pagination as MantinePagination } from '@mantine/core';
 import '@mantine/core/styles.css';
 import styles from '../main.module.scss';
 import paginationStyles from './Pagination.module.scss';
+interface PaginationProps {
+  onChangePage: (page: number) => void;
+  pageCount: number;
+  defaultValue: number;
+}
 
-export default function Pagination() {
+export default function Pagination({ onChangePage, pageCount, defaultValue }: PaginationProps) {
   return (
     <div className={`${styles.pagination} ${paginationStyles.paginationWrapper}`}>
-      <MantinePagination total={5} defaultValue={1} />
+      <MantinePagination total={pageCount} defaultValue={defaultValue} onChange={onChangePage} />
     </div>
   );
 }
