2d8cd609d674ffb699183051908ea0b734d77cd4
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-22 14:51:32 +0900

    #30 refactor: AvailableTimeList 모듈화

---
+++ src/app/components/reservation/AvailableTimeList.tsx
@@ -0,0 +1,38 @@
+import { Schedule } from '@/types/schedule';
+import S from './AvailableTimeList.module.scss';
+import classNames from 'classnames';
+import dayjs from 'dayjs';
+
+interface AvailableTimeListProps {
+  selectedDate: Date;
+  selectedTimeId: number | null;
+  schedule: Schedule[] | undefined;
+  handleTimeSelect: (id: number) => void;
+}
+
+export default function AvailableTimeList({
+  schedule,
+  selectedDate,
+  selectedTimeId,
+  handleTimeSelect,
+}: AvailableTimeListProps) {
+  const availableTimesOfSelectedDate =
+    schedule
+      ?.filter(schedule => schedule.date === dayjs(selectedDate).format('YYYY-MM-DD')) // 스케쥴 배열에서 선택된 날짜에 맞는 스케쥴만 필터링한 뒤, times 프로퍼티의 값만 걸러낸다.
+      .map(schedule => schedule.times) // times가 배열이라 times끼리 모으면 2차원 배열이되므로 flat으로 한 겹 벗겨내어 1차원 객체 배열로 만들어준다.
+      .flat() || [];
+
+  return (
+    <div className={S.availableTimeList}>
+      {availableTimesOfSelectedDate.map(time => (
+        <div
+          key={time.id}
+          className={classNames(S.availableTime, { [S.selectedTime]: selectedTimeId === time.id })}
+          onClick={() => handleTimeSelect(time.id)}
+        >
+          {time.startTime} ~ {time.endTime}
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/components/reservation/ReservationSelector.tsx
+++ src/app/components/reservation/ReservationSelector.tsx
@@ -3,6 +3,7 @@ import S from './ReservationSelector.module.scss';
 import dayjs from 'dayjs';
 import classNames from 'classnames';
 import ReservationCalendar from './ReservationCalendar';
+import AvailableTimeList from './AvailableTimeList';
 
 interface ReservationSelectorProps {
   activityId: number;
@@ -24,12 +25,6 @@ export default function ReservationSelector({
   const date = selectedDate.getDate();
   const { data: schedule } = useAvailableSchedule({ activityId, year, month, date });
   const availableDates = schedule?.map(schedule => schedule.date) || []; // 예약 가능일자 배열
-
-  const availableTimesOfSelectedDate =
-    schedule
-      ?.filter(schedule => schedule.date === dayjs(selectedDate).format('YYYY-MM-DD')) // 스케쥴 배열에서 선택된 날짜에 맞는 스케쥴만 필터링한 뒤, times 프로퍼티의 값만 걸러낸다.
-      .map(schedule => schedule.times) // times가 배열이라 times끼리 모으면 2차원 배열이되므로 flat으로 한 겹 벗겨내어 1차원 객체 배열로 만들어준다.
-      .flat() || [];
 
   const getDayProps = (date: Date) => {
     const formattedDate = dayjs(date).format('YYYY-MM-DD');
@@ -53,17 +48,12 @@ export default function ReservationSelector({
 
       <section className={S.availableTimeContainer}>
         <p className={S.sectionTitle}>예약 가능한 시간</p>
-        <div className={S.availableTimeList}>
-          {availableTimesOfSelectedDate.map(time => (
-            <div
-              key={time.id}
-              className={classNames(S.availableTime, { [S.selectedTime]: selectedTimeId === time.id })}
-              onClick={() => handleTimeSelect(time.id)}
-            >
-              {time.startTime} ~ {time.endTime}
-            </div>
-          ))}
-        </div>
+        <AvailableTimeList
+          selectedDate={selectedDate}
+          schedule={schedule}
+          handleTimeSelect={handleTimeSelect}
+          selectedTimeId={selectedTimeId}
+        />
         <div className={S.separator} />
       </section>
     </>
