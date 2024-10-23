bbaf65dea7ccbe8ce3c0e647120075ab411bcdac
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-23 13:13:24 +0900

    #30 refactor: 예약 모달 모듈화

--- src/app/components/reservation/CalendarModal.tsx
+++ src/app/components/reservation/CalendarModal.tsx
@@ -1,26 +1,33 @@
 import S from './CalendarModal.module.scss';
 import ReservationSelector from './ReservationSelector';
-import Modal from '../@shared/modal/Modal';
 import { useToggle } from '@/hooks/useToggle';
 import dayjs from 'dayjs';
 import { useReservationStore } from '@/stores/useReservationStore';
+import ReservationModal from './ReservationModal';
 
 export default function CalendarModal({ activityId }: { activityId: number }) {
   const { selectedDate, selectedTime } = useReservationStore(state => state.reservation);
   const { toggleValue, toggleDispatch } = useToggle();
+  const selectDateModalButtonText = selectedTime
+    ? `${dayjs(selectedDate).format('YYYY/MM/DD')} ${selectedTime.startTime} ~ ${selectedTime.endTime}`
+    : '날짜 선택하기';
 
   return (
     <>
       <p className={S.sectionTitle}>날짜</p>
-      <button className={S.openCalendarButton} onClick={() => toggleDispatch({ type: 'switch' })}>
-        {selectedTime
-          ? `${dayjs(selectedDate).format('YYYY/MM/DD')} ${selectedTime.startTime} ~ ${selectedTime.endTime}`
-          : '날짜 선택하기'}
-      </button>
-      <Modal isOpen={toggleValue} onClose={() => toggleDispatch({ type: 'off' })} width="480px" className={S.modal}>
-        <p className={S.modalTitle}>날짜 및 시간 선택</p>
+
+      <ReservationModal
+        triggerButtonText={selectDateModalButtonText}
+        isModalOpen={toggleValue}
+        modalTitle="날짜 및 시간 선택"
+        onCloseModal={() => toggleDispatch({ type: 'off' })}
+        onTriggerButtonClick={() => toggleDispatch({ type: 'on' })}
+        triggerButtonClassName={S.openCalendarButton}
+        modalHeight="auto"
+        modalWidth="auto"
+      >
         <ReservationSelector activityId={activityId} />
-      </Modal>
+      </ReservationModal>
     </>
   );
 }
--- src/app/components/reservation/MobileReservation.tsx
+++ src/app/components/reservation/MobileReservation.tsx
@@ -5,14 +5,17 @@ import HeadCountStepper from './HeadCountStepper';
 import Button from '../@shared/button/Button';
 import { useToggle } from '@/hooks/useToggle';
 import ReservationSelector from './ReservationSelector';
-import Modal from '../@shared/modal/Modal';
 import dayjs from 'dayjs';
 import { useReservationStore } from '@/stores/useReservationStore';
+import ReservationModal from './ReservationModal';
 
 export default function MobileReservation({ price, activityId }: ReservationProps) {
   const { headCount, selectedDate, selectedTime } = useReservationStore(state => state.reservation);
-  const { toggleValue: isCalendarModalOpen, toggleDispatch: calendarModalToggleDispatch } = useToggle();
+  const { toggleValue: isSelectDateModalOpen, toggleDispatch: SelectDateModalToggleDispatch } = useToggle();
   const { toggleValue: isStepperModalOpen, toggleDispatch: stepperModalToggleDispatch } = useToggle();
+  const selectDateModalButtonText = selectedTime
+    ? `${dayjs(selectedDate).format('YYYY/MM/DD')} ${selectedTime.startTime} ~ ${selectedTime.endTime}`
+    : '날짜 선택하기';
 
   const totalPrice = `₩ ${getCurrencyFormat(price * headCount)}`;
   return (
@@ -20,63 +23,28 @@ export default function MobileReservation({ price, activityId }: ReservationProp
       <section className={S.selectorContainer}>
         <div className={S.priceAndHeadCount}>
           <p className={S.price}>{totalPrice} /</p>
-          <button
-            className={S.headCountButton}
-            onClick={() => stepperModalToggleDispatch({ type: 'on' })}
-          >{`총 ${headCount}인`}</button>
+          <ReservationModal
+            triggerButtonText={`총 ${headCount}인`}
+            isModalOpen={isStepperModalOpen}
+            modalTitle="인원 선택"
+            onCloseModal={() => stepperModalToggleDispatch({ type: 'off' })}
+            onTriggerButtonClick={() => stepperModalToggleDispatch({ type: 'on' })}
+            triggerButtonClassName={S.headCountButton}
+          >
+            <HeadCountStepper />
+          </ReservationModal>
         </div>
-        <button className={S.selectDateButton} onClick={() => calendarModalToggleDispatch({ type: 'on' })}>
-          {selectedTime
-            ? `${dayjs(selectedDate).format('YYYY/MM/DD')} ${selectedTime.startTime} ~ ${selectedTime.endTime}`
-            : '날짜 선택하기'}
-        </button>
+        <ReservationModal
+          triggerButtonText={selectDateModalButtonText}
+          isModalOpen={isSelectDateModalOpen}
+          modalTitle="날짜 및 시간 선택"
+          onCloseModal={() => SelectDateModalToggleDispatch({ type: 'off' })}
+          onTriggerButtonClick={() => SelectDateModalToggleDispatch({ type: 'on' })}
+          triggerButtonClassName={S.selectDateButton}
+        >
+          <ReservationSelector activityId={activityId} />
+        </ReservationModal>
       </section>
-
-      <Modal
-        isOpen={isCalendarModalOpen}
-        onClose={() => calendarModalToggleDispatch({ type: 'off' })}
-        width="100dvw"
-        height="100dvh"
-        className={S.modal}
-      >
-        <p className={S.modalTitle}>날짜 및 시간 선택</p>
-        <ReservationSelector activityId={activityId} />
-
-        <Button
-          borderRadius="radius4"
-          buttonColor="nomadBlack"
-          padding="padding14"
-          textSize="md"
-          className={S.modalCloseButton}
-          onClick={() => calendarModalToggleDispatch({ type: 'off' })}
-        >
-          확인
-        </Button>
-      </Modal>
-
-      <Modal
-        isOpen={isStepperModalOpen}
-        onClose={() => stepperModalToggleDispatch({ type: 'off' })}
-        width="100dvw"
-        height="100dvh"
-        className={S.modal}
-        showCloseButton={true}
-      >
-        <p className={S.modalTitle}>인원 선택</p>
-        <HeadCountStepper />
-
-        <Button
-          borderRadius="radius4"
-          buttonColor="nomadBlack"
-          padding="padding14"
-          textSize="md"
-          className={S.modalCloseButton}
-          onClick={() => stepperModalToggleDispatch({ type: 'off' })}
-        >
-          확인
-        </Button>
-      </Modal>
-
       <Button
         borderRadius="radius4"
         buttonColor="nomadBlack"
