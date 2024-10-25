4f8eb482944892938674be1484aa96d2755f4d26
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-25 13:39:53 +0900

    #30 fix: useReservationMutation에서 쿼리 클라이언 사용 부분 수정
    - const queryClient = new QueryClient();
      - 전역에서 Provider를 만들 때 사용.
    - const queryClient = useQueryClient();
      - 이미 만들어진 전역 QueryClient를 가져 옴.

--- src/queries/useReservationsMutation.ts
+++ src/queries/useReservationsMutation.ts
@@ -1,10 +1,10 @@
 import { AVAILABLE_SCHEDULE_QUERY_KEY } from '@/constants/queryKeys';
 import { submitReservation } from '@/fetches/submitReservation';
 import { SubmitReservationParams } from '@/types/reservation';
-import { QueryClient, useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useReservationMutation = () => {
-  const queryClient = new QueryClient();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ activityId, scheduleId, headCount }: SubmitReservationParams) =>
       submitReservation({ activityId, scheduleId, headCount }),
