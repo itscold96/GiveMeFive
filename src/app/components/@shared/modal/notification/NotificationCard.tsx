c2a6ce026590052b725ebf4f3a11ab3d99b9d16a
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-25 17:06:10 +0900

    #43 feat: 승인, 거절 시에 인디케이터 및 텍스트 하이라이트 기능 구현

--- src/app/components/@shared/modal/notification/NotificationCard.tsx
+++ src/app/components/@shared/modal/notification/NotificationCard.tsx
@@ -3,6 +3,7 @@ import closeImg from '@/images/icons/icon-close-gray.svg';
 import Image from 'next/image';
 import getElapsedPeriod from '@/utils/getElapsedPeriod';
 import { useNotificationDelete } from '@/queries/useNotificationDelete';
+import classNames from 'classnames';
 
 interface NotificationCardProps {
   id: number;
@@ -16,10 +17,18 @@ export default function NotificationCard({ id, message, createdAt }: Notificatio
     mutate();
   };
 
+  const isDeclined = message.includes('거절');
+  const [before, after] = message.split(/거절|승인/);
+
   return (
     <div className={S.notificationContainer}>
+      <div className={classNames(S.messageTypeIndicator, { [S.declined]: isDeclined })} />
       <div className={S.closeButtonWrapper}>
-        <div className={S.message}>{message}</div>
+        <div className={S.message}>
+          {before}
+          {isDeclined ? <span className={S.declined}>거절</span> : <span className={S.confirmed}>승인</span>}
+          {after}
+        </div>
         <button className={S.closeButton} onClick={handleDeleteClick}>
           <Image src={closeImg} alt="닫기 이미지" width={24} height={24} />
         </button>
