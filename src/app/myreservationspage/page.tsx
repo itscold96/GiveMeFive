1b859b44e20b0db8d48e2b40cb670e6c6177a71c
MELATONIN99 <sindogfood@naver.com>
2024-11-02 16:04:56 +0900

    #47 move: 불필요 페이지 파일, 폴더 삭제

--- src/app/myreservationspage/page.tsx
+++
@@ -1,9 +0,0 @@
-import Reservations from '../components/myReservations/Reservations/Reservations';
-
-export default function MyReservationsPage() {
-  return (
-    <div>
-      <Reservations />
-    </div>
-  );
-}
