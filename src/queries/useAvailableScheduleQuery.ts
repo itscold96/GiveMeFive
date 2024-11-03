1078643593e4af1fae2c848f0c22d00d3c3178e6
Minchan <98478661+itscold96@users.noreply.github.com>
2024-11-03 22:41:03 +0900

    #75 fix: useAvailableScheduleQuery stailtime 제거 및 쿼리키에 activityId, year 추가
    서로 다른 체험 페이지에서 쿼리가 캐싱되어 문제가 발생하는 것을 확인하여 수정.
    placeholderData로 새로운 데이터를 받아오기 전 이전 데이터를 유지하여 화면 깜빡임 최소화

--- src/queries/useAvailableScheduleQuery.ts
+++ src/queries/useAvailableScheduleQuery.ts
@@ -12,9 +12,9 @@ export const useAvailableSchedule = ({ activityId, month, year, date }: useAvail
     // 날짜를 바꾸는 사이에 다른 사람의 예약이 이루어질 수 있으므로
     // 최대한 사용자가 예약하기를 눌렀을 때, 이미 예약된 일정이 되는 것을 피하기 위해
     // 선택된 날짜가 바뀌면 새로 서버에서 데이터를 받아오도록 queryKey에 date 추가
-    queryKey: [USERS_QUERY_KEY, date, month],
+    queryKey: [USERS_QUERY_KEY, activityId, year, date, month],
     queryFn: () => getAvailableSchedule({ activityId, month, year }),
-    staleTime: 60 * 1000,
+    placeholderData: previousData => previousData,
     select: data => {
       const today = dayjs();
       // 서버에서 받아온 예약 날짜들 중에서 오늘과 그 이후 스케쥴만 뽑아서 전달
