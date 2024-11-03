15aa5d0547f7f474810a7a0be1d57830fea4a815
Minchan <98478661+itscold96@users.noreply.github.com>
2024-11-03 22:39:01 +0900

    #75 fix: zustand 전역 스토어에 의해 컴포넌트 언마운트 후에도 유지되던 상태 수정
    컴포넌트 언마운트 시, 예약 제출 시에 전역 상태 초기화

--- src/hooks/useReservationSubmit.ts
+++ src/hooks/useReservationSubmit.ts
@@ -8,6 +8,7 @@ import { useToggle } from './useToggle';
 // 다른 컴포넌트를 사용해야하므로 중복되는 예약 제출 로직을 모듈화
 export const useReservationSubmit = (activityId: number) => {
   const { headCount, selectedTime } = useReservationStore(state => state.reservation);
+  const { reset } = useReservationStore(state => state.action);
   const [modalMessage, setModalMessage] = useState('');
   const { toggleValue: isModalOpen, toggleDispatch: modalToggle } = useToggle();
   const { mutateAsync } = useReservationMutation(); // 성공, 실패에 따라 다른 modal 창을 열어야 하므로 비동기 처리가 필요함
@@ -17,6 +18,7 @@ export const useReservationSubmit = (activityId: number) => {
       try {
         await mutateAsync({ activityId, headCount, scheduleId: selectedTime.id });
         setModalMessage('예약이 완료되었습니다');
+        reset();
       } catch (error) {
         if (error instanceof AxiosError) {
           if (error.status === 401) {
