2954cafd45c4580e53ce44bacfc742db0cc668eb
ayoooyh <syh9566@naver.com>
2024-11-04 23:41:27 +0900

    #46 fix : homeLaout 수정

--- src/app/(contents)/layout.tsx
+++ src/app/(contents)/layout.tsx
@@ -2,8 +2,8 @@ import Footer from '@/app/components/@shared/footer/Footer';
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {children}
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: 'calc(100vh - 70px)' }}>
+      <div style={{ flex: 1 }}>{children}</div>
       <Footer />
     </div>
   );
