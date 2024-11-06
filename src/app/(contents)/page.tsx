cfd24f829aab791941a0b7206bc55bd37bced975
ayoooyh <syh9566@naver.com>
2024-11-06 19:57:49 +0900

    #46 fix : Loader 공통컴포넌트 분리 및 메인페이지 적용

--- src/app/(contents)/page.tsx
+++ src/app/(contents)/page.tsx
@@ -1,29 +1,11 @@
 import Main from './mainpage/main';
 import { Suspense } from 'react';
-import { Loader } from '@mantine/core';
+import Loader from '@/app/components/@shared/loader/Loader';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Suspense
-        fallback={
-          <div
-            style={{
-              position: 'fixed',
-              width: '100vw',
-              height: '100vh',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              backgroundColor: 'white',
-              left: 0,
-              top: 0,
-            }}
-          >
-            <Loader size="lg" />
-          </div>
-        }
-      >
+      <Suspense fallback={<Loader />}>
         <Main />
       </Suspense>
     </div>
