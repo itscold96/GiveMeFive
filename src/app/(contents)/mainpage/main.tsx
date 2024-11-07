72b1100d3b0a6db605e57bcabf1e3eaebe3e6180
ayoooyh <syh9566@naver.com>
2024-11-07 22:58:53 +0900

    #46 fix : 메인페이지 초기 로딩 로더 수정

--- src/app/(contents)/mainpage/main.tsx
+++ src/app/(contents)/mainpage/main.tsx
@@ -5,6 +5,7 @@ import AllZoneCard from './allzonecard/AllZoneCard';
 import Banner from './banner/Banner';
 import Search from './search/Search';
 import { getActivities } from '@/fetches/activities';
+import Loading from '@/app/loading';
 
 export default async function Main() {
   const activitiesData = await getActivities({
@@ -25,20 +26,8 @@ export default async function Main() {
     <div>
       <Banner bestActivity={firstBestActivity} />
       <div className={S.mainContainer}>
-        {/* Search는 독립적으로 동작하므로 별도 Suspense 유지 */}
-        <Suspense fallback={<div className={S.searchLoading}>검색창 로딩중..</div>}>
+        <Suspense fallback={<Loading />}>
           <Search />
-        </Suspense>
-
-        {/* BestZoneCard와 AllZoneCard는 활동 데이터를 표시하는 
-            관련 컴포넌트이므로 하나의 Suspense로 묶음 */}
-        <Suspense
-          fallback={
-            <div className={S.contentLoading}>
-              <p>활동 정보를 불러오는 중입니다..</p>
-            </div>
-          }
-        >
           <BestZoneCard initialBestActivitiesData={bestActivitiesData} />
           <AllZoneCard initialActivitiesData={activitiesData} />
         </Suspense>
